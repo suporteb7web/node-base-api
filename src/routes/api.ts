5c38e36facee491026b6bba82e5a14e8f3aec536
bonieky <bonieky@gmail.com>
2021-09-24 17:59:47 -0300

    moving ping route from routes file to server file

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -4,8 +4,6 @@ import * as ApiController from '../controllers/apiController';
 
 const router = Router();
 
-router.get('/ping', ApiController.ping);
-
 router.post('/register', ApiController.register);
 router.post('/login', ApiController.login);
 
--- src/server.ts
+++ src/server.ts
@@ -11,18 +11,20 @@ const server = express();
 server.use(cors());
 
 server.use(express.static(path.join(__dirname, '../public')));
-server.use(express.urlencoded({extended: true}));
+server.use(express.urlencoded({ extended: true }));
+
+server.get('/ping', (req: Request, res: Response) => res.json({ pong: true }));
 
 server.use(apiRoutes);
 
 server.use((req: Request, res: Response) => {
     res.status(404);
-    res.json({error: 'Endpoint não encontrado.'});
+    res.json({ error: 'Endpoint não encontrado.' });
 });
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
     res.status(400); // Bad Request
-    console.log( err );
+    console.log(err);
     res.json({ error: 'Ocorreu algum erro.' });
 }
 server.use(errorHandler);
