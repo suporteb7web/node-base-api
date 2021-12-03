dbb331b5be38e333c11375233cb58e071340f97e
Marcelo Santos <rocha.mer21@gmail.com>
2021-12-03 12:08:59 -0300

    projeto limpo

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,12 +1,5 @@
-import { Router } from 'express';
+import { Router } from 'express'
 
-import * as ApiController from '../controllers/apiController';
+const router = Router()
 
-const router = Router();
-
-router.post('/register', ApiController.register);
-router.post('/login', ApiController.login);
-
-router.get('/list', ApiController.list);
-
-export default router;
+export default router
